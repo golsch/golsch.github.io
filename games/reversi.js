bc6af1847a476962903903ab5540a480aba85fcb
legose95 <lennard.golsch@icloud.com>
2018-04-07 12:25:46 +0200

    working hover

--- games/reversi.js
+++ games/reversi.js
@@ -1,6 +1,7 @@
 const NUMBER_OF_COLS = 8;
 const NUMBER_OF_ROWS = 8;
-const BLOCK_SIZE = 99;
+const SHADOW_SIZE = 1;
+const BLOCK_SIZE = 100 - SHADOW_SIZE;
 const COLOR_BOARD = "white";
 const COLOR_PLAYER_ONE = "red";
 const COLOR_PLAYER_TWO = "green";
@@ -11,71 +12,85 @@ var ctx = null;
 var canvas = null;
 
 var current_color = null;
+var color_temp = null;
+
+var x_temp = null;
+var y_temp = null;
+
+
 var current_x = -1;
 var current_y = -1;
-var clicked = false;
+
+var game_config = new Array();
+
+function buildStartConfig() {
+    for (var i = 0; i < NUMBER_OF_COLS*NUMBER_OF_ROWS; i++) {
+        if(i == 27 || i == 36) {
+            game_config[i] = COLOR_PLAYER_ONE;
+        } 
+        else if (i == 28 || i == 35) {
+            game_config[i] = COLOR_PLAYER_TWO;
+        }
+        else {
+            game_config[i] = COLOR_BOARD;
+        }
+        
+    }
+}
+
+function coordinatesToBlock(x,y) {
+    return x*NUMBER_OF_ROWS+y;
+}
 
 function screenToBlock(x, y) {
     var block =  {
         "row": Math.floor(y / BLOCK_SIZE),
         "col": Math.floor(x / BLOCK_SIZE)
     };
-
     return block;
 }
 
-
-
 function drawBlock(x, y, color) {
     ctx.fillStyle=color;
-    ctx.fillRect(x*(BLOCK_SIZE+1), y*(BLOCK_SIZE+1), BLOCK_SIZE, BLOCK_SIZE);
+    ctx.fillRect(y*(BLOCK_SIZE+SHADOW_SIZE), x*(BLOCK_SIZE+SHADOW_SIZE), BLOCK_SIZE, BLOCK_SIZE);
 }
-
 
 function drawBoard() {
-    for (var x = 0; x < NUMBER_OF_ROWS; x++) {
-        for (var y = 0; y < NUMBER_OF_ROWS; y++) {
-            drawBlock(x, y, COLOR_BOARD);
+    buildStartConfig();
+    var x = 0;
+    var y = 0;
+    for (var i = 0; i < NUMBER_OF_COLS*NUMBER_OF_ROWS; i++) {
+        if(x == NUMBER_OF_COLS) {
+            y++;
+            x = 0;
         }
+        drawBlock(x, y, game_config[i]);
+        x++;
     }
-    
-    board_start_config();
 }
 
-
 function board_click(ev) {
-    !clicked;
     current_color = COLOR_PLAYER_ONE;
     var x = ev.clientX - canvas.offsetLeft,
     y = ev.clientY - canvas.offsetTop,
     clickedBlock = screenToBlock(x, y);
     drawBlock(clickedBlock.col, clickedBlock.row, COLOR_PLAYER_ONE);
-    !clicked;
+    get_color(x, y);
 }
-
-function board_start_config() {
-    drawBlock(4,4,COLOR_PLAYER_ONE);
-    drawBlock(3,3,COLOR_PLAYER_ONE);
-    drawBlock(3,4,COLOR_PLAYER_TWO);
-    drawBlock(4,3,COLOR_PLAYER_TWO);
-}
-
 
 function board_hover(ev) {
-    if(!clicked) {
-        var x = ev.clientX - canvas.offsetLeft,
-        y = ev.clientY - canvas.offsetTop,
-        clickedBlock = screenToBlock(x, y);
-
-        if(current_x == clickedBlock.row && current_y == clickedBlock.col) {
-            drawBlock(clickedBlock.col, clickedBlock.row, COLOR_HOVER)
-        }
-        else {
-            drawBlock(current_y, current_x, COLOR_PLAYER_ONE)
-        }
-
+    var x = ev.clientX - canvas.offsetLeft;
+    var y = ev.clientY - canvas.offsetTop;
+    clickedBlock = screenToBlock(x, y);
+    //field-switch
+    if(current_x != clickedBlock.row || current_y != clickedBlock.col) {
+        drawBlock(current_x, current_y,game_config[coordinatesToBlock(current_x,current_y)]);
         current_x = clickedBlock.row;
         current_y = clickedBlock.col;
+        //field is not a player
+        if(game_config[coordinatesToBlock(current_x,current_y)]==COLOR_BOARD) {
+            drawBlock(current_x, current_y, COLOR_HOVER); 
+        }
     }
 }
 
@@ -84,7 +99,7 @@ function draw() {
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
         drawBoard();
-        canvas.addEventListener('click', board_click, false);
+        //canvas.addEventListener('click', board_click, false);
         canvas.addEventListener("mousemove", board_hover);
     } else {
         alert("Canvas not supported!");
