5b4fe9642f9d478c9d0b3796c9f21ea553828a1c
legose95 <lennard.golsch@icloud.com>
2018-04-09 13:31:34 +0200

    fixed blocked fields

--- games/reversi.js
+++ games/reversi.js
@@ -1,18 +1,19 @@
-const NUMBER_OF_COLS = 8;
+const COLS = 6 + 2;
 //quadratic field
-const NUMBER_OF_ROWS = NUMBER_OF_COLS;
+const ROWS = COLS;
 const SHADOW_SIZE = 1;
 const COLOR_BOARD = "white";
 const COLOR_PLAYER_ONE = "red";
 const COLOR_PLAYER_TWO = "green";
 const COLOR_HOVER = "grey";
-const DIRECTIONS =new Array("LEFT","RIGHT","UP", "DOWN", "UPLEFT", "UPRIGHT", "DOWNLEFT", "DOWNRIGHT");
-//------------------------------------
+const COLOR_BLOCKED = "black";
+const DIRECTIONS = new Array("LEFT","RIGHT","UP", "DOWN", "UPLEFT", "UPRIGHT", "DOWNLEFT", "DOWNRIGHT");
+//---------------
 var current_color = null;
 var current_row = -1;
 var current_col = -1;
 var current_player = COLOR_PLAYER_ONE;
-var current_config = new Array();
+var map = new Array();
 //------------------------------------
 var ctx = null;
 var canvas = null;
@@ -21,23 +22,37 @@ var canvas_init_size = 0;
 //------------------------------------
 
 //build gameconfig with initial start options
-function build_start_config() {
-    for (var index = 0; index < NUMBER_OF_COLS * NUMBER_OF_ROWS; index++) {
-        if(index == ((NUMBER_OF_ROWS / 2) - 1)*NUMBER_OF_ROWS+NUMBER_OF_COLS / 2 - 1 || index ==(NUMBER_OF_ROWS / 2)*NUMBER_OF_ROWS + NUMBER_OF_COLS / 2) {
-            current_config[index] = COLOR_PLAYER_ONE;
-        } 
-        else if (index == ((NUMBER_OF_ROWS / 2)) * NUMBER_OF_ROWS + NUMBER_OF_COLS / 2 - 1 || index ==((NUMBER_OF_ROWS / 2) - 1) * NUMBER_OF_ROWS + NUMBER_OF_COLS / 2) {
-            current_config[index] = COLOR_PLAYER_TWO;
+function generate_map() {
+    for (var index = 0; index < COLS * ROWS; index++) {
+        var col = get_col(index);
+        var row = get_row(index);
+        
+        if(col == 0 || col == COLS - 1 || row == 0 || row == ROWS - 1) {
+            map[index] = COLOR_BLOCKED;
         }
         else {
-            current_config[index] = COLOR_BOARD;
+            if(index == ((ROWS / 2) - 1)* ROWS + COLS / 2 - 1 || index == (COLS / 2) * ROWS + COLS / 2) {
+                map[index] = COLOR_PLAYER_ONE;
+            } else if (index == ((COLS / 2)) * ROWS + COLS / 2 - 1 || index == ((ROWS / 2) - 1) * ROWS + COLS / 2) {
+                map[index] = COLOR_PLAYER_TWO;
+            } else {
+                map[index] = COLOR_BOARD;
+            }
         }
     }
 }
 
-//calc block to index
+ function get_row(index) {
+		return parseInt(index / ROWS);
+}
+
+//get col with index
+function get_col(index) {
+		return index % COLS;
+}
+
 function block_to_index(x, y) {
-    return x * NUMBER_OF_COLS + y;
+    return x * COLS + y;
 }
 
 //calc screen to block
@@ -51,16 +66,6 @@ function screen_to_block(x, y) {
 function draw_block(x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x * (block_size + SHADOW_SIZE), y * (block_size + SHADOW_SIZE), block_size, block_size);
-}
-
-//get row with index
- function get_row(index) {
-		return parseInt(index / NUMBER_OF_COLS);
-}
-
-//get col with index
-function get_col(index) {
-		return index % NUMBER_OF_COLS;
 }
 
 //switch player current player
@@ -77,19 +82,21 @@ function switch_player() {
 function draw_board() {
     var x = 0;
     var y = 0;
-    for (var i = 0; i < NUMBER_OF_COLS*NUMBER_OF_ROWS; i++) {
-        if(x == NUMBER_OF_COLS) {
-            y++;
-            x = 0;
+    for (var index = 0; index < COLS * ROWS; index++) {
+        if(map[index] != COLOR_BLOCKED) {
+            if(x == COLS - 2) {
+                y++;
+                x = 0;
+            }
+            draw_block(y, x, map[index]);
+            x++;
         }
-        draw_block(y, x, current_config[i]);
-        x++;
     }
 }
 
 //check block if it is playerblock
 function is_player(index){
-    if(current_config[index] != COLOR_BOARD) {
+    if(map[index] == COLOR_PLAYER_ONE || map[index] == COLOR_PLAYER_TWO) {
         return true;
     }
     return false;
@@ -97,27 +104,29 @@ function is_player(index){
 
 //check direction if its flippable
 function is_flippable(index, player, direction) {
-		var flipsStones = false;
-		while (is_player(index) && current_config[index] != player) {
-			index = index_to_direction(index, direction);
-			flipsStones = true;
-		}
-		//if the index ended on a field of the player, the row can be flipped.
-		if (current_config[index] == player && flipsStones){
-			return true;
-		}
-		return false;
+    //console.log("is flippable?");
+    var flipsStones = false;
+    while (is_player(index) && map[index] != player) {
+        index = index_to_direction(index, direction);
+        flipsStones = true;
+    }
+    //console.log("yes");
+    //if the index ended on a field of the player, the row can be flipped.
+    if (map[index] == player && flipsStones){
+        return true;
+    }
+    return false;
 }
 
 //flip stones in direction
 function flip(index, player, direction) {
-		index = index_to_direction(index, direction);
-		if(is_flippable(index, player, direction)) {
-			while(is_player(index) && current_config[index]!=player) {
-				current_config[index] = player;
-				index = index_to_direction(index, direction);
-			}
-		}
+    index = index_to_direction(index, direction);
+    if(is_flippable(index, player, direction)) {
+        while(is_player(index) && map[index]!=player) {
+            map[index] = player;
+            index = index_to_direction(index, direction);
+        }
+    }
 }
 
 //add new move
@@ -125,7 +134,7 @@ function add(index, player) {
     for(var i = 0; i < DIRECTIONS.length; i++) {
         flip(index, player, DIRECTIONS[i]);
     }
-    current_config[index] = player;
+    map[index] = player;
     draw_block(get_row(index), get_col(index), player);
     
 }
@@ -135,12 +144,12 @@ function index_to_direction(index, direction) {
     switch(direction) {
 		case "LEFT": return --index;
 		case "RIGHT": return ++index;
-		case "UP": return index - NUMBER_OF_COLS;
-		case "DOWN": return index + NUMBER_OF_COLS;
-		case "UPLEFT": return index - NUMBER_OF_COLS - 1;
-		case "UPRIGHT": return index - NUMBER_OF_COLS + 1;
-		case "DOWNLEFT": return index + NUMBER_OF_COLS - 1;
-		case "DOWNRIGHT": return index + NUMBER_OF_COLS + 1;
+		case "UP": return index - COLS;
+		case "DOWN": return index + COLS;
+		case "UPLEFT": return index - COLS - 1;
+		case "UPRIGHT": return index - COLS + 1;
+		case "DOWNLEFT": return index + COLS - 1;
+		case "DOWNRIGHT": return index + COLS + 1;
 		
 		default: alert("error, unkonwn direction");
     }
@@ -152,9 +161,9 @@ function board_click(ev) {
     y = ev.clientY - canvas.offsetTop,
     clickedBlock = screen_to_block(x, y);
     //check that block is empty
-    if(!is_player(block_to_index(clickedBlock.row, clickedBlock.col))) {
-        current_config[block_to_index(clickedBlock.row, clickedBlock.col)] = current_player;
-        add(block_to_index(clickedBlock.row, clickedBlock.col), current_player);
+    if(!is_player(block_to_index(clickedBlock.row + 1, clickedBlock.col + 1))) {
+        map[block_to_index(clickedBlock.row + 1, clickedBlock.col + 1)] = current_player;
+        add(block_to_index(clickedBlock.row + 1, clickedBlock.col + 1), current_player);
         draw_board();
         switch_player();
     }
@@ -170,11 +179,11 @@ function board_hover(ev) {
     clickedBlock = screen_to_block(x, y);
     //field-switch
     if(current_row != clickedBlock.row || current_col != clickedBlock.col) {
-        draw_block(current_row, current_col,current_config[block_to_index(current_row,current_col)]);
+        draw_block(current_row, current_col,map[block_to_index(current_row + 1,current_col + 1)]);
         current_row = clickedBlock.row;
         current_col = clickedBlock.col;
         //field is not a player
-        if(current_config[block_to_index(current_row,current_col)]==COLOR_BOARD) {
+        if(map[block_to_index(current_row + 1,current_col + 1)]==COLOR_BOARD) {
             draw_block(current_row, current_col, COLOR_HOVER); 
         }
     }
@@ -182,7 +191,7 @@ function board_hover(ev) {
 
 //event when mouse leaves canvas
 function mouse_out() {
-    draw_block(current_row, current_col,current_config[block_to_index(current_row,current_col)]);
+    draw_block(current_row, current_col,map[block_to_index(current_row + 1,current_col + 1)]);
 }
 
 //window resize event
@@ -201,7 +210,7 @@ function renew_canvas_size() {
         ctx.canvas.height = ctx.canvas.width;
     }
         
-    block_size = (canvas.width/NUMBER_OF_COLS)-SHADOW_SIZE;
+    block_size = (canvas.width/(COLS - 2)) - SHADOW_SIZE;
     draw_board();
 }
 
@@ -211,7 +220,7 @@ function draw() {
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
         canvas_init_size = canvas.width;
-        build_start_config();
+        generate_map();    
         renew_canvas_size();
         canvas.addEventListener('click', board_click, false);
         canvas.addEventListener("mousemove", board_hover);
